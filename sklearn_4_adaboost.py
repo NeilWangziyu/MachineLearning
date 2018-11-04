284845f521c4c0ae239cd61120a988d2c8007210
Ziyu <16078718+NeilWangziyu@users.noreply.github.com>
2018-11-04 16:46:11 +0800

    2018-11-4

--- sklearn_4_adaboost.py
+++ sklearn_4_adaboost.py
@@ -1,8 +1,17 @@
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, cross_val_predict
 from sklearn.datasets import load_iris
 from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
 
 iris = load_iris()
 clf = AdaBoostClassifier(n_estimators=100)
 scores = cross_val_score(clf, iris.data, iris.target, cv=5)
 print(scores.mean())
+
+y_train_pred = cross_val_predict(clf, iris.data, iris.target, cv=5)
+conf_mx = confusion_matrix(iris.target, y_train_pred)
+print(conf_mx)
+
+plt.matshow(conf_mx)
+plt.show()
